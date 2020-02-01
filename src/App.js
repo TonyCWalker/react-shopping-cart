22e7fb5791675e4770e424b806ac2a324ba28ef2
Tony Walker <Walkertony509@gmail.com>
2020-01-31 17:07:43 -0800

    I have created the CartContext file, updated the navigation and shopping cart components, as well as updated the app.js.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
 	const [products] = useState(data);
@@ -21,10 +23,11 @@ function App() {
 	return (
 		<div className="App">
 		  <ProductContext.Provider value={{ products, addItem }}>
-			<Navigation cart={cart} />
-			{/* Routes */}
-			<Route exact path="/" component={Products} /> />
-			<Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+		  <CartContext.Provider value={cart}>
+          <Navigation />
+          <Route exact path="/" component={Products} /> />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
 		  </ProductContext.Provider>
 		</div>
 	  );
